fa5b7dff86d5d72124608b218a03fe3baeb3f4c0
Keijiro Takahashi <keijiro@X1.jt.unity3d.com>
2015-04-27 14:48:53 +0900

    Initial commit.

---
+++ Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Test : MonoBehaviour
+{
+    public TextureFormat textureFormat;
+
+    void Start()
+    {
+        var tex = new Texture2D(4, 1, textureFormat, false);
+        tex.filterMode = FilterMode.Point;
+        tex.wrapMode = TextureWrapMode.Clamp;
+
+        Byte[] data = {
+            0xff, 0, 0, 0,
+            0, 0xff, 0, 0,
+            0, 0, 0xff, 0,
+            0, 0, 0, 0xff
+        };
+
+        tex.LoadRawTextureData(data);
+        tex.Apply();
+
+        GetComponent<Renderer>().material.mainTexture = tex;
+    }
+}
